9edb7657f475bd1a46b6990f7db1ca5dc3289a97
CamiloTello002 <camilojtello5@gmail.com>
2025-04-16 20:06:45 -0500

    feat: add metadescription to website
    This will help with SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,8 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "Professional Developer Portfolio",
-  description: "A full stack developer portfolio showcasing projects and skills",
-    generator: 'v0.dev'
+  title: "Camilo Tello",
+  description: " Full- stack developer specializing in building scalable web applications with modern technologies.Strong focus on frontend development using React, Next.js, TypeScript, and TailwindCSS, with experience optimizing performance through server components, responsive design, and error handling.Proficient in backend development with Node.js, Supabase, and PostgresSQL, as well as containerized deployments with Docker.Passionate about delivering efficient, user - friendly solutions with a keen eye for UI / UX and SEO optimization. ",
 }
 
 export default function RootLayout({
