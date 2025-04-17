8cecaf7c53d1d97326ecc5edd9abe05025416f2d
CamiloTello002 <camilojtello5@gmail.com>
2025-04-16 20:17:11 -0500

    feat: add favicon to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata = {
   title: "Camilo Tello",
   description: " Full- stack developer specializing in building scalable web applications with modern technologies.Strong focus on frontend development using React, Next.js, TypeScript, and TailwindCSS, with experience optimizing performance through server components, responsive design, and error handling.Proficient in backend development with Node.js, Supabase, and PostgresSQL, as well as containerized deployments with Docker.Passionate about delivering efficient, user - friendly solutions with a keen eye for UI / UX and SEO optimization. ",
+  icons: {
+    icon: "/favicon.ico",
+  }
 }
 
 export default function RootLayout({
@@ -17,6 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
